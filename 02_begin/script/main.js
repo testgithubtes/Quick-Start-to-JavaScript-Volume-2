be440412c60c5e8e6528e336a3a4dcd7856d42f7
tomm <so4raycpw@moakt.co>
2017-10-28 11:32:52 +0200

    Lesson2 video done

---
+++ 02_begin/script/main.js
@@ -0,0 +1,12 @@
+var sign = prompt("what is your fav movie?").toLowerCase();
+switch(sign){
+  case "star wars":
+    alert("nice Adventure movie");
+    break;
+  case "jhony wicker":
+    alert("nice action movie");
+    break;
+  default:
+    alert("nice choose ");
+    break;
+}
